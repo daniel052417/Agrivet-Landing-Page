5a8fa4d2d2e524b438b43ab93aa1520868cf408b
daniel052417 <danieljohn.pepito24@gmail.com>
2025-10-23 16:49:02 +0800

    debug

--- landing-page/vite.config.ts
+++ landing-page/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Agrivet-Landing-Page/', 
   plugins: [react()],
   server: {
     port: 3000,
